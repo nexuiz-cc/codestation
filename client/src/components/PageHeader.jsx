798a3b254df8b8dfac23e39350db0b9c9b83eaea
nexuiz-cc <41991563+nexuiz-cc@users.noreply.github.com>
2023-09-28 23:53:05 +0900

    ..

--- client/src/components/PageHeader.jsx
+++ client/src/components/PageHeader.jsx
@@ -1,10 +1,12 @@
-import { Button ,Modal} from 'antd';
+/* eslint-disable react/prop-types */
+/* eslint-disable react/destructuring-assignment */
+import { Button, Modal } from 'antd';
 import { PlusOutlined } from '@ant-design/icons';
-import React,{ useState } from 'react';
-import styles from "../css/PageHeader.module.css"
+import React, { useState } from 'react';
+import styles from '../css/PageHeader.module.css';
 
 function PageHeader(props) {
-    const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
   const showModal = () => {
     setIsModalOpen(true);
   };
@@ -14,21 +16,21 @@ function PageHeader(props) {
   const handleCancel = () => {
     setIsModalOpen(false);
   };
-    return (
-        <div className={styles.row}>
-            <div className={styles.pageHeader}>
-                {props.title}
-            </div>
-            {props.children}
-            <Button onClick={showModal}><PlusOutlined /></Button>
-            <Modal title="Basic Modal" open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
+  return (
+    <div className={styles.row}>
+      <div className={styles.pageHeader}>
+        {props.title}
+      </div>
+      {props.children}
+      <Button onClick={showModal}><PlusOutlined /></Button>
+      <Modal title="Basic Modal" open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
         <p>Some contents...</p>
         <p>Some contents...</p>
         <p>Some contents...</p>
       </Modal>
-        </div>
+    </div>
 
-    );
+  );
 }
 
 export default PageHeader;
