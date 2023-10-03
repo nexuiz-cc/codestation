2f66c18fcc66f8755b21c79f2ab6989b9be4eca1
nexuiz-cc <41991563+nexuiz-cc@users.noreply.github.com>
2023-10-03 17:20:14 +0900

    ...

--- client/src/pages/MovieDetail.jsx
+++ client/src/pages/MovieDetail.jsx
@@ -1,48 +1,65 @@
 import axios from 'axios';
 import React, { useState, useEffect } from 'react';
+import { Card } from 'antd';
 import { useParams } from 'react-router-dom';
 import '../css/movieDetail.css';
 import _ from 'lodash';
 function MovieDetail() {
   const { id } = useParams();
   const [data, setData] = useState({});
-  const [date,setDate]= useState('');
-  const [year,setYear] = useState('');
+  const [date, setDate] = useState('');
+  const [year, setYear] = useState('');
+  const [actors, setActors] = useState({});
   useEffect(() => {
-    let detailURL = 'https://api.themoviedb.org/3/movie/';    
+    let detailURL = 'https://api.themoviedb.org/3/movie/';
     const header = {
       accept: 'application/json',
       Authorization:
         'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkMWUwNzhiMmQ5Mjg4YWMzMWE2NThkNzAzNmQzNzJmYSIsInN1YiI6IjY0Y2U1YzA1NGQ2NzkxMDEzOWVlMDIyZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.4Wx-Ezz67V6JInCIDSOCnkA9H0seIBhpOupm_Amj4Co',
     };
-    axios.get(detailURL + id+'?language=en-US', { headers: header }).then(function (response) {
+    axios.get(detailURL + id + '?language=en-US', { headers: header }).then(function (response) {
       response.data.poster_path = 'https://image.tmdb.org/t/p/w185' + response.data.poster_path;
       setData(response.data);
-      console.log('data:',response.data);
-      
+      console.log('data:', response.data);
     });
 
-    axios.get(detailURL + id+'/release_dates', { headers: header }).then(function (response) {
-        let str = response.data.results[0].release_dates[0].release_date;
-        console.log(response.data.results[0].release_dates[0].release_date);
-        setDate(_.replace(str,'T00:00:00.000Z',''));
-        setYear(str.substring(0,4));
-        console.log('date:',date,year);
-      });
-    
+    axios.get(detailURL + id + '/release_dates', { headers: header }).then(function (response) {
+      let str = response.data.results[0].release_dates[0].release_date;
+      console.log(response.data.results[0].release_dates[0].release_date);
+      setDate(_.replace(str, 'T00:00:00.000Z', ''));
+      setYear(str.substring(0, 4));
+      console.log('date:', date, year);
+    });
+
+    axios.get(detailURL + id + '/credits', { headers: header }).then(function (response) {
+      for (let item of response.data.cast) {
+        item.profile_path = 'https://image.tmdb.org/t/p/w185' + item.profile_path;
+      }
+      setActors(response.data.cast);
+      console.log('actors:', response.data);
+    });
   }, []);
 
   return (
     <div className='box'>
       <img
-        src={'https://image.tmdb.org/t/p/original' + data.backdrop_path}
+        src={'https://image.tmdb.org/t/p/original/' + data.backdrop_path}
         className='backdrop'
       />
       <div className='photo'>
         <img src={data.poster_path} />
-        <h4 className='h4'>  {data.title} ( {year} )  </h4>
+        <h4 className='h4'>
+          {' '}
+          {data.title} ( {year} ){' '}
+        </h4>
         <p className='text'>{date}</p>
         <p className='overview'>{data.overview}</p>
+      </div>
+      <div>
+      
+        <Card className='card'>
+        
+        </Card>
       </div>
     </div>
   );
